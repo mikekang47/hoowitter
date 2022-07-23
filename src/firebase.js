234b93661107bd79f530babf7dced2ba42d290ac
mikekang47 <gyeong0308@gmail.com>
2022-07-23 14:00:46 +0900

    Initailize firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAoNC51EBSRTAtr7slc_D3rAZcv26BzAYM",
+    authDomain: "hoowitter-4b338.firebaseapp.com",
+    projectId: "hoowitter-4b338",
+    storageBucket: "hoowitter-4b338.appspot.com",
+    messagingSenderId: "25493157914",
+    appId: "1:25493157914:web:344d35cf94a26f555db9a3",
+    measurementId: "G-2Z95WCX6K8"
+  };
+  
+  // Initialize Firebase
+export default initializeApp(firebaseConfig);
+
+  
